2363a84fe7fd99cd9649651c31881713315c41ef
mariocstweb <mariocstmra@gmail.com>
2024-02-07 13:42:49 +0100

    added store.js

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import axios from "axios";
+import { store } from "./data/store.js";
+const endpoint = "";
 export default {
   name: "Boolfix",
+  data: () => ({
+
+  }),
   components: { AppHeader, AppMain }
 }
 </script>
